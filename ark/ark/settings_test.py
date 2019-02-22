58ccee43aa2961f37f06bf6a129c106248b9a5ce
zjy <zhujiangyou88@163.com>
2019-02-22 14:57:09 +0800

    z

---
+++ ark/ark/settings_test.py
@@ -0,0 +1,20 @@
+from .settings import *
+
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ark',
+        'PORT':'3306',
+        'USER':'root',
+        'PASSWORD':'Quattro!',
+    }
+}
+
+
+STATIC_ROOT = '/var/ark/static'
+MEDIA_ROOT = '/var/ark/media'
